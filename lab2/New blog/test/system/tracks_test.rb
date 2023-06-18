ac3f361a6154357ae31494192ca32f7f9184a590
nahed <nahed@gmail.com>
2023-06-19 01:19:06 +0300

    lab2 commit

---
+++ lab2/New blog/test/system/tracks_test.rb
@@ -0,0 +1,43 @@
+require "application_system_test_case"
+
+class TracksTest < ApplicationSystemTestCase
+  setup do
+    @track = tracks(:one)
+  end
+
+  test "visiting the index" do
+    visit tracks_url
+    assert_selector "h1", text: "Tracks"
+  end
+
+  test "should create track" do
+    visit tracks_url
+    click_on "New track"
+
+    fill_in "Description", with: @track.description
+    fill_in "Name", with: @track.name
+    click_on "Create Track"
+
+    assert_text "Track was successfully created"
+    click_on "Back"
+  end
+
+  test "should update Track" do
+    visit track_url(@track)
+    click_on "Edit this track", match: :first
+
+    fill_in "Description", with: @track.description
+    fill_in "Name", with: @track.name
+    click_on "Update Track"
+
+    assert_text "Track was successfully updated"
+    click_on "Back"
+  end
+
+  test "should destroy Track" do
+    visit track_url(@track)
+    click_on "Destroy this track", match: :first
+
+    assert_text "Track was successfully destroyed"
+  end
+end
